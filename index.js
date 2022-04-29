2bf19ad1cc2570510afeeedbbf2745c37a274625
Brandon <brandon07000@gmail.com>
2022-04-29 11:55:28 +0200

    changement du port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const exhibitionRouter = require('./routes/exhibition.router');
 const app = express();
 const auth = require('./auth');
 const User = require('./model/user.model');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.DB_CONNECTION)
 .then((res) => console.log('Connected to database'))
